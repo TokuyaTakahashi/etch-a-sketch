d0d0e2a7cc3002a0efda864f33574f6c71b5e555
Tokuya Takahashi <tokuyatakahashi@gmail.com>
2021-11-24 20:26:33 -0500

    Updated UI and added functionality(darker color button)

--- script.js
+++ script.js
@@ -8,19 +8,28 @@ function generateGrid(pixelsNum) {
     }
     calcPixSize(pixelsNum);
     eventListeners();
+    const p = document.createElement('p');
+    const div = document.getElementById('gridsize');
+    p.textContent = `${Math.sqrt(pixelsNum)} x ${Math.sqrt(pixelsNum)}`;
+    div.appendChild(p);
 }
 
 function eventListeners () {
     var pixels = document.querySelectorAll('.pixel');
-        pixels.forEach(pixel => pixel.addEventListener('mouseover', hover));
+        pixels.forEach(pixel => pixel.addEventListener('mouseover', hover));  
     var clearbutton = document.querySelector('#clear');
         clearbutton.addEventListener('click',clear);
     var newgridbutton = document.querySelector('#new-grid');
         newgridbutton.addEventListener('click', newgrid);
     var bar = document.getElementById('range');
         bar.oninput = slider;
-    var random = document.querySelector('#random-color');
-        random.addEventListener('click', randomColor);    
+    var random = document.querySelector('#random-button');
+        random.addEventListener('click', buttonclick); 
+    var darker = document.querySelector('#darker-button');
+        darker.addEventListener('click', buttonclick);    
+    var black = document.querySelector('#black-button');
+        black.addEventListener('click', buttonclick); 
+    
 }
 
 function calcPixSize(pixelsNum) {
@@ -31,42 +40,75 @@ function calcPixSize(pixelsNum) {
         pixel.style.height = `${pixeldimen}px`;
     });
 }
+function buttonclick(e) {
+    addborder(this);
+}
 
-function slider(e) {
-    const container = document.querySelector('.field');
-    while(container.firstChild) {
-        container.removeChild(container.firstChild);
+function addborder(button) { 
+    var random = document.getElementById('random-button');
+    var darker = document.getElementById('darker-button');
+    var black = document.getElementById('black-button');
+    while(darker.classList.contains("clicked") || black.classList.contains("clicked") || random.classList.contains("clicked")) {
+        darker.classList.remove("clicked");
+        random.classList.remove("clicked");
+        black.classList.remove("clicked");
     }
+    button.classList.add("clicked");
+}
+function slider(e) {
+    remove();
     generateGrid(Math.pow(e.target.value, 2));
 }
-function randomColor(e) {
-    var pixels = document.querySelectorAll('randomColor');
-}
+
 function hover(e) {
     if(document.getElementById('random-color').checked) {
-        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        const randomColor = Math.random().toString(16).substring(2,8);
         this.style.backgroundColor = "#" + randomColor;
+    }
+    else if(document.getElementById('darker-color').checked) {
+        if(this.style.backgroundColor != "") {
+            var color = rgbconvert(this.style.backgroundColor);
+            this.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+        } else {
+            const randomColor = Math.random().toString(16).substring(2,8);
+            this.style.backgroundColor = "#" + randomColor;
+        }
     } else {
         this.style.backgroundColor = "black";
     }
 }
-
+function rgbconvert(rgb) {
+    var regex = /\d+/g;
+    var newArr = rgb.match(regex).map(value => parseInt(value) - (.1*255));
+    return newArr;
+}
 function clear(e) {
     var pixels = document.querySelectorAll('.pixel');
     pixels.forEach(pixel => {
         pixel.style.backgroundColor = "";
     });
 }
-
-function newgrid(e) {
-    var gridsize = prompt("Enter new grid size");
+function remove() {
     const container = document.querySelector('.field');
     while(container.firstChild) {
         container.removeChild(container.firstChild);
     }
+    const div = document.getElementById('gridsize');
+    if(div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
+}
+function newgrid(e) {
+    do {
+       var gridsize = prompt("Enter new grid size (Max: 50)");
+    }
+    while(gridsize > 50 || gridsize < 0) 
+    var bar = document.getElementById('range')
+    bar.value = gridsize;
+    remove();
     generateGrid(Math.pow(gridsize, 2));
 }
-generateGrid(16*16);
+generateGrid(16);
 
 
 
